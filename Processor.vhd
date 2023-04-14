6b7c695706a2c43660c8fe9c311a6ddc61a4c45e
عمر شريف على شبل الزهار <14712020102094@stud.cu.edu.eg>
2023-04-14 17:22:52 +0200

    processor semi integaration

--- Processor.vhd
+++ Processor.vhd
@@ -11,12 +11,19 @@ entity Processor is
 end Processor;
 
 architecture ProcessorArch of Processor is
+--signal cont_en : std_logic;
+signal addAmt : std_logic_vector(1 downto 0);
+signal curr_intsr : std_logic_vector(15 downto 0);
+signal instr : std_logic_vector(31 downto 0);
+signal fdout : std_logic_vector(31 downto 0);
+
+
 begin
-pc: entity work.pc();
-FetchUnit: entity work.FetchUnit();
-RegFile: entity work.MyMemory();
-FD_Buffer: entity work.MynBuffer();
-DE_Buffer: entity work.MynBuffer();
+pc: entity work.pc port map(clk=>clk, rst=>rst, en=>'1', addAmt =>addAmt , ci=>curr_instr);
+FetchUnit: entity work.FetchUnit port map(clk=>clk, rst=>rst, currInstrPc=>curr_instr, instr=>instr, pcNxtAddAmt=>addAmt);
+RegFile: entity work.MyMemory port map();
+FD_Buffer: entity work.MynBuffer generic map (32) port map(clk => clk, rst => rst, en=>'1' , d=>instr , q=>fdout);
+DE_Buffer: entity work.MynBuffer port map();
 
 
 end architecture;
