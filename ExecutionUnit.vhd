9894be03575f2f0e6a24b6a72ec5e34262dd5bc1
Moaaz Tarek <moaaztarek@hotmail.com>
2023-04-15 22:26:53 +0200

    ExecutionUnit opcode operations

--- ExecutionUnit.vhd
+++ ExecutionUnit.vhd
@@ -15,15 +15,61 @@ end ExecutionUnit;
 
 architecture ExecutionUnitArch of ExecutionUnit is
 begin
-process(clk)
-variable opCodeint:integer;
-begin
-opCodeint:=to_integer(unsigned(ALUop));
-if(rising_edge(clk)) then 
-    case opCodeint is
-        when 0 => res<=(others=>'0');
-        when others=> res<=(others=>'0'); 
-    end case;
-end if;
+    process(clk)
+    variable opCodeint:integer;
+    variable result:std_logic_vector(16 downto 0);
+    begin
+        opCodeint:=to_integer(unsigned(ALUop));
+        if(rising_edge(clk)) then 
+            case opCodeint is
+                -- NOP
+                when 0 => res<=(others=>'0');
+                -- SETC
+                when 1 => 
+                    res<=(others=>'0');
+                    CCr(0) <= '1';
+                -- CLRC
+                when 2 => 
+                    res<=(others=>'0');
+                    CCr(0) <= '0';
+                -- NOT Rdst, Rsrc1
+                when 3 => res<= not src1;
+                -- INC Rdst, Rsrc1
+                when 4 => res<= std_logic_vector(unsigned(src1) + 1);
+                -- DEC Rdst, Rsrc1
+                when 5 => res<= std_logic_vector(unsigned(src1) - 1);
+                -- ADD Rdst, Rsrc1, Rsrc2
+                when 9 => 
+                    result := std_logic_vector(unsigned(src1) + unsigned(src2));
+                    res<= result;
+                    CCR(0) <= result(16);
+                    CCR(1) <= res(15);
+                    CCR(2) <= '1' when result(15 downto 0) = others => '0' else '1';
+
+                -- SUB Rdst, Rsrc1, Rsrc2
+                when 10 => 
+                    result := std_logic_vector(unsigned(src1) - unsigned(src2));
+                    res<= result;
+                    CCR(0) <= result(16);
+                    CCR(1) <= res(15);
+                    CCR(2) <= '1' when result(15 downto 0) = others => '0' else '1';
+                -- AND Rdst, Rsrc1, Rsrc2
+                when 11 => 
+                    result := src1 and src2;
+                    res<= result;
+                    CCR(1) <= res(15);
+                    CCR(2) <= '1' when result(15 downto 0) = others => '0' else '1';
+                -- OR Rdst, Rsrc1, Rsrc2
+                when 12 => 
+                    result := src1 or src2;
+                    res<= result;
+                    CCR(1) <= res(15);
+                    CCR(2) <= '1' when result(15 downto 0) = others => '0' else '1';
+                -- JZ Rdst
+                -- JC Rdst
+                -- IADD Rdst, Rsrc1, Imm
+                when others=> res<=(others=>'0'); 
+            end case;
+        end if;
     end process;
 end architecture;
