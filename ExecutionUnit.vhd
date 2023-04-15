b9f716f76e875faa176f12af42284a2edd081b60
Moaaz Tarek <moaaztarek@hotmail.com>
2023-04-15 23:25:26 +0200

    Ex continued
    maybe will produce latches.
    pc updates not done.
    i think should enter rdst and output new one in push and pop operations

--- ExecutionUnit.vhd
+++ ExecutionUnit.vhd
@@ -8,67 +8,159 @@ entity ExecutionUnit is
         src1,src2,imm: in std_logic_vector(15 downto 0);
         ALUsrc,RegDst: in std_logic;
         inPort: in std_logic_vector(15 downto 0);
-        res:out std_logic_vector(15 downto 0);
-        CCR: out std_logic_vector(2 downto 0) -- Z(zero flag) | N(Negative flag) | C(Carry Flag)	
+        outPort: out std_logic_vector(15 downto 0);
+        res1, res2:out std_logic_vector(15 downto 0);
+        CCRout: out std_logic_vector(2 downto 0); -- Z(zero flag) | N(Negative flag) | C(Carry Flag)
+        CCRin: in std_logic_vector(2 downto 0); -- Z(zero flag) | N(Negative flag) | C(Carry Flag)
+        SPin: in std_logic_vector(15 downto 0);
+        SPout: out std_logic_vector(15 downto 0)
     );
 end ExecutionUnit;
 
 architecture ExecutionUnitArch of ExecutionUnit is
 begin
     process(clk)
-    variable opCodeint:integer;
-    variable result:std_logic_vector(16 downto 0);
+    variable opCodeint: integer;
+    variable result: std_logic_vector(16 downto 0);
+    variable SPplus1: std_logic_vector(15 downto 0);
     begin
         opCodeint:=to_integer(unsigned(ALUop));
         if(rising_edge(clk)) then 
             case opCodeint is
                 -- NOP
-                when 0 => res<=(others=>'0');
+                when 0 => res1<=(others=>'0');
                 -- SETC
                 when 1 => 
-                    res<=(others=>'0');
-                    CCr(0) <= '1';
+                    CCRout(0) <= '1';
                 -- CLRC
                 when 2 => 
-                    res<=(others=>'0');
-                    CCr(0) <= '0';
+                    CCRout(0) <= '0';
                 -- NOT Rdst, Rsrc1
-                when 3 => res<= not src1;
+                when 3 => res1<= not src1;
                 -- INC Rdst, Rsrc1
-                when 4 => res<= std_logic_vector(unsigned(src1) + 1);
+                when 4 => res1<= std_logic_vector(unsigned(src1) + 1);
                 -- DEC Rdst, Rsrc1
-                when 5 => res<= std_logic_vector(unsigned(src1) - 1);
+                when 5 => res1<= std_logic_vector(unsigned(src1) - 1);
+                -- OUT Rsrc1
+                when 6 => outPort<= src1;
+                -- IN Rdst
+                when 7 => res1<= inPort;
+                -- MOV Rdst, Rsrc1
+                when 8 => res1<= src1;
                 -- ADD Rdst, Rsrc1, Rsrc2
                 when 9 => 
-                    result := std_logic_vector(unsigned(src1) + unsigned(src2));
-                    res<= result;
-                    CCR(0) <= result(16);
-                    CCR(1) <= res(15);
-                    CCR(2) <= '1' when result(15 downto 0) = others => '0' else '1';
+                    result := std_logic_vector(unsigned('0' & src1) + unsigned('0' & src2));
+                    res1<= result(15 downto 0);
+                    CCRout(0) <= result(16);
+                    CCRout(1) <= result(15);
+                    if (result(15 downto 0) = (15 downto 0 => '0'))
+                    then
+                        CCRout(2) <= '1';
+                    else
+                        CCRout(2) <= '0';
+                    end if;
 
                 -- SUB Rdst, Rsrc1, Rsrc2
                 when 10 => 
-                    result := std_logic_vector(unsigned(src1) - unsigned(src2));
-                    res<= result;
-                    CCR(0) <= result(16);
-                    CCR(1) <= res(15);
-                    CCR(2) <= '1' when result(15 downto 0) = others => '0' else '1';
+                    result := std_logic_vector(unsigned('0' & src1) - unsigned('0' & src2));
+                    res1<= result (15 downto 0);
+                    CCRout(0) <= result(16);
+                    CCRout(1) <= result(15);
+                    if (result(15 downto 0) = (15 downto 0 => '0'))
+                    then
+                        CCRout(2) <= '1';
+                    else
+                        CCRout(2) <= '0';
+                    end if;
                 -- AND Rdst, Rsrc1, Rsrc2
                 when 11 => 
-                    result := src1 and src2;
-                    res<= result;
-                    CCR(1) <= res(15);
-                    CCR(2) <= '1' when result(15 downto 0) = others => '0' else '1';
+                    result := '0' & src1 and '0' & src2;
+                    res1<= result (15 downto 0);
+                    CCRout(1) <= result(15);
+                    if (result(15 downto 0) = (15 downto 0 => '0'))
+                    then
+                        CCRout(2) <= '1';
+                    else
+                        CCRout(2) <= '0';
+                    end if;
                 -- OR Rdst, Rsrc1, Rsrc2
                 when 12 => 
-                    result := src1 or src2;
-                    res<= result;
-                    CCR(1) <= res(15);
-                    CCR(2) <= '1' when result(15 downto 0) = others => '0' else '1';
+                    result := '0' & src1 or '0' & src2;
+                    res1<= result (15 downto 0);
+                    CCRout(1) <= result(15);
+                    if (result(15 downto 0) = (15 downto 0 => '0'))
+                    then
+                        CCRout(2) <= '1';
+                    else
+                        CCRout(2) <= '0';
+                    end if;
+                -- PUSH Rsrc1
+                when 13 => 
+                    res1 <= src1;
+                    res2 <= SPin; -- destination
+                    SPout <= std_logic_vector(unsigned(SPin) - 1);
+                -- POP Rdst
+                when 14 =>
+                    res1 <= src1; -- destination
+                    res2 <= std_logic_vector(unsigned(SPin) + 1);
+                    SPout <= std_logic_vector(unsigned(SPin) + 1);
+                -- LDD Rdst, Rsrc1
+                when 15 => res1<= imm;
+                -- STD Rsrc2, Rsrc1
+                when 16 => 
+                    res1<= src1;
+                    res2<= src2;
                 -- JZ Rdst
+                when 17 => 
+                    if(CCRin(2)='1') then
+                        res1<= src1;
+                        CCRout(2) <= '0';
+                    end if;
                 -- JC Rdst
+                when 18 => 
+                    if(CCRin(0)='1') then
+                        res1<= src1;
+                        CCRout(0) <= '0';
+                    end if;
+                -- JMP Rdst
+                when 19 => res1<= src1;
+                -- CALL Rdst
+                when 20 => 
+                    -- should update pc
+                    res1<= src1;
+                    res2<= SPin;
+                    SPout <= std_logic_vector(unsigned(SPin) - 1);
+
+                -- RET
+                when 21 => 
+                    -- should update pc
+                    res1<= src1;
+                    res2<= std_logic_vector(unsigned(SPin) + 1);
+                    SPout <= std_logic_vector(unsigned(SPin) + 1);
+                -- RTI
+                when 22 =>
+                    -- should update pc
+                    -- should update flags
+                    res1<= src1;
+                    res2<= std_logic_vector(unsigned(SPin) + 1);
+                    SPout <= std_logic_vector(unsigned(SPin) + 1);
+                -- restore flags
                 -- IADD Rdst, Rsrc1, Imm
-                when others=> res<=(others=>'0'); 
+                when 23 => 
+                    result := std_logic_vector(unsigned('0' & src1) + unsigned('0' & imm));
+                    res1<= result(15 downto 0);
+                    CCRout(0) <= result(16);
+                    CCRout(1) <= result(15);
+                    if (result(15 downto 0) = (15 downto 0 => '0'))
+                    then
+                        CCRout(2) <= '1';
+                    else
+                        CCRout(2) <= '0';
+                    end if;
+                -- LDM Rdst, Imm
+                when 24 => res1<= imm;
+
+                when others=> res1<=(others=>'0'); 
             end case;
         end if;
     end process;
