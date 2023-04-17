d64edbe92991a18ec0a2ca15b19b9a25be32a9a3
عمر شريف على شبل الزهار <14712020102094@stud.cu.edu.eg>
2023-04-17 23:20:00 +0200

    semi integration

--- Processor.vhd
+++ Processor.vhd
@@ -24,11 +24,15 @@ signal ALUop : std_logic_vector(4 downto 0);
 signal ALUsrc,RegDst,MEMWrite,MEMRead,MemtoReg,RegWrite : std_logic;
 signal dein : std_logic_vector(74 downto 0);
 signal deout : std_logic_vector(74 downto 0);
-signal Alures : std_logic_vector(15 downto 0);
-signal AluCCR : std_logic_vector(2 downto 0);
-signal emin : std_logic_vector(38 downto 0)
-signal emout : std_logic_vector(38 downto 0);
-signal read_port : std_logic_vector(15 downto 0);
+signal DataRes : std_logic_vector(15 downto 0);
+signal Memadd : std_logic_vector(15 downto 0);
+signal AluCCRout : std_logic_vector(2 downto 0);
+signal AluCCRin : std_logic_vector(2 downto 0) := "000";
+signal SPin : std_logic_vector(15 downto 0);
+signal SPout : std_logic_vector(15 downto 0);
+signal emin : std_logic_vector(35 downto 0)
+signal emout : std_logic_vector(35 downto 0);
+signal read_data : std_logic_vector(15 downto 0);
 signal mwbin : std_logic_vector(17 downto 0);
 signal mwbout : std_logic_vector(17 downto 0);
 -----------------------------------------------
@@ -53,14 +57,19 @@ signal mwbout : std_logic_vector(17 downto 0);
 --0                 --RegWrite
 -----------------------------------------------
 ------------------emin-------------------------
---38 downto 23      --write data  (rt)
---22 downto 7       --ALU result  (16 downto 7) accessable
---6 downto 4        --CCR
+--35 downto 20      --DataRes
+--19 downto 4       --Memadd    (13 downto 4)
 --3                 --MEMWrite
 --2                 --MEMRead
 --1                 --MemtoReg
 --0                 --RegWrite
 -----------------------------------------------
+------------------mwbin------------------------
+--33 downto 18       --DataRes
+--17 downto 2        --read_data
+--1                  --MemtoReg
+--0                  --RegWrite
+
 begin
 pc: entity work.pc port map(clk=>clk, rst=>rst, en=>'1', addAmt =>addAmt , ci=>curr_instr);
 FetchUnit: entity work.FetchUnit port map(clk=>clk, rst=>rst, currInstrPc=>curr_instr, instr=>instr, pcNxtAddAmt=>addAmt);
@@ -70,14 +79,13 @@ RegFile: entity work.MyMemory generic map (16,3) port map(clk => clk, rst => rst
 ControlUnit: entity work.ControlUnit port map(opcode => fdout(31 downto 27), AlUop => AlUop, AlUsrc => AlUsrc, RegDst => RegDst, MEMWrite => MEMWrite, MEMRead => MEMRead, MemtoReg => MemtoReg, RegWrite => RegWrite);
 dein <= fdout(47 downto 32) & fdout(15 downto 0) & read_port_rs & read_port_rt & AlUop & AlUsrc & RegDst & MEMWrite & MEMRead & MemtoReg & RegWrite;
 DE_Buffer: entity work.MynBuffer generic map (59) port map(clk => clk , rst => rst, en => '1', d => dein, q => deout);
-ExecutionUnit: entity work.ExecutionUnit port map(clk => clk, ALUop => deout(10 downto 6), src1 => deout(42 downto 27) ,src2 => deout(26 downto 11), imm => deout(58 downto 43), ALUsrc => deout(5), RegDst => deout(4),inPort => inPort, res => Alures, CCR => AluCCR, PCin => deout(74 downto 59));
-emin <= read_port_rt & Alures & AluCCR & deout(3 downto 0);
-EM_Buffer: entity work.MynBuffer generic map (39) port map(clk => clk , rst => rst, en => '1', d => emin, q => emout);
-MemoryUnit: entity work.MemoryUnit generic map (16,10) port map(clk => clk, rst => rst, en=>'1', r_add1 => emout(16 downto 7), w_add => emout(16 downto 7),read_en => emout(2), w_en => emout(3), write_port => emout(38 downto 23), read_port => read_port);
-------problem in generic map for address size 16 or 10 bits?????
-------read add and write add are same?????????????
-------some control signal are not input to mydatamemory
-mwbin <= read_port & emout(1 downto 0);
-MWB_Buffer: entity work.MynBuffer generic map (18) port map(clk => clk , rst => rst, en => '1', d => mwbin, q => mwbout);
+CCR_Buffer: entity work.MynBuffer generic map (15) port map(clk => clk , rst => rst, en => '1', d => AluCCRout, q => AluCCRin);
+SP_Buffer: entity work.MynBuffer generic map (15) port map(clk => clk , rst => rst, en => '1', d => SPout, q => SPin);
+ExecutionUnit: entity work.ExecutionUnit port map(clk => clk, ALUop => deout(10 downto 6), src1 => deout(42 downto 27) ,src2 => deout(26 downto 11), imm => deout(58 downto 43), ALUsrc => deout(5), RegDst => deout(4),inPort => inPort, datares => DataRes, memadd => Memadd, CCRout => AluCCRout, CCRin => AluCCRin, SPin =>SPin, SPout=> SPout, PCin => deout(74 downto 59));
+emin <= DataRes & Memadd & deout(3 downto 0);
+EM_Buffer: entity work.MynBuffer generic map (36) port map(clk => clk , rst => rst, en => '1', d => emin, q => emout);
+MemoryUnit: entity work.MemoryUnit generic map (16,10) port map(clk => clk, rst => rst, en=>'1', r_add1 => emout(13 downto 4), w_add => emout(13 downto 4),read_en => emout(2), w_en => emout(3), write_data => emout(35 downto 20), read_data => read_data);
+mwbin <= DataRes & read_data & emout(1 downto 0);
+MWB_Buffer: entity work.MynBuffer generic map (34) port map(clk => clk , rst => rst, en => '1', d => mwbin, q => mwbout);
 
 end architecture;
