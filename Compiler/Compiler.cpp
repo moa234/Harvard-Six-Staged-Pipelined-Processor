da5ea52398fe6b6216eb72bca413af7eb626af20
Moaaz Tarek <moaaztarek@hotmail.com>
2023-04-24 17:33:44 +0200

    syntax erors

--- Compiler/Compiler.cpp
+++ Compiler/Compiler.cpp
@@ -41,94 +41,144 @@ string hex2bin (char x) {
     return {};
 }
 
-int main (int argc, char *argv[]) {
-    ifstream fin(argv[1] + string(".asm"));
-    ofstream fout(argv[2] + string(".mem"));
+void encode (const vector<string> &op, const vector<string> &reg, ofstream &fout) {
+    for (int i = 0; i < op.size(); i++) {
+        fout << "\t" << i << ": ";
+        if (op[i] == "NOP") {
+            fout << "0000000000000000" << endl;
+        } else if (op[i] == "SETC") {
+            fout << "0000100000000000" << endl;
+        } else if (op[i] == "CLRC") {
+            fout << "0001000000000000" << endl;
+        } else if (op[i] == "NOT") {
+            fout << "00011" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << "00000"
+                 << endl;
+        } else if (op[i] == "INC") {
+            fout << "00100" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << "00000"
+                 << endl;
+        } else if (op[i] == "DEC") {
+            fout << "00101" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << "00000"
+                 << endl;
+        } else if (op[i] == "OUT") {
+            fout << "00110000" << bitset<3>(reg[i][1] - '0').to_string() << "00000" << endl;
+        } else if (op[i] == "IN") {
+            fout << "00111" << bitset<3>(reg[i][1] - '0').to_string() << "00000000" << endl;
+        } else if (op[i] == "MOV") {
+            fout << "01000" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << "00000"
+                 << endl;
+        } else if (op[i] == "ADD") {
+            fout << "01001" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << bitset<3>(reg[i][7] - '0').to_string() << "00" << endl;
+        } else if (op[i] == "SUB") {
+            fout << "01010" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << bitset<3>(reg[i][7] - '0').to_string() << "00" << endl;
+        } else if (op[i] == "AND") {
+            fout << "01011" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << bitset<3>(reg[i][7] - '0').to_string() << "00" << endl;
+        } else if (op[i] == "OR") {
+            fout << "01100" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << bitset<3>(reg[i][7] - '0').to_string() << "00" << endl;
+        } else if (op[i] == "PUSH") {
+            fout << "01101000" << bitset<3>(reg[i][1] - '0').to_string() << "00000" << endl;
+        } else if (op[i] == "POP") {
+            fout << "01110" << bitset<3>(reg[i][1] - '0').to_string() << "00000000" << endl;
+        } else if (op[i] == "LDD") {
+            fout << "01111" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string()
+                 << "00000"
+                 << endl;
+        } else if (op[i] == "STD") {
+            fout << "10000000" << bitset<3>(reg[i][4] - '0').to_string() << bitset<3>(reg[i][1] - '0').to_string()
+                 << "00" << endl;
+        } else if (op[i] == "JZ") {
+            fout << "10001" << bitset<3>(reg[i][1] - '0').to_string() << "00000000" << endl;
+        } else if (op[i] == "JC") {
+            fout << "10010" << bitset<3>(reg[i][1] - '0').to_string() << "00000000" << endl;
+        } else if (op[i] == "JMP") {
+            fout << "10011" << bitset<3>(reg[i][1] - '0').to_string() << "00000000" << endl;
+        } else if (op[i] == "CALL") {
+            fout << "10100" << bitset<3>(reg[i][1] - '0').to_string() << "00000000" << endl;
+        } else if (op[i] == "RET") {
+            fout << "1010100000000000" << endl;
+        } else if (op[i] == "RTI") {
+            fout << "1011000000000000" << endl;
+        } else if (op[i] == "IADD") {
+            fout << "1" << bitset<3>(reg[i][1] - '0').to_string() << bitset<3>(reg[i][4] - '0').to_string() << "00000"
+                 << endl;
+            fout << hex2bin(reg[i][6]) + hex2bin(reg[i][7]) + hex2bin(reg[i][8]) + hex2bin(reg[i][9]) << endl;
+        }
+    }
+}
+
+void checksyntax (const string &op) {
+    if (op != "NOP" && op != "SETC" && op != "CLRC" && op != "RET" && op != "RTI" && op != "INC" && op != "DEC" &&
+        op != "OUT" && op != "IN" && op != "PUSH" && op != "POP" && op != "JZ" && op != "JC" && op != "JMP" &&
+        op != "CALL" && op != "IADD" && op != "MOV" && op != "ADD" && op != "SUB" && op != "AND" && op != "OR" &&
+        op != "LDD" && op != "STD" && op != "NOT" && op != "LDM" && op != "CALL") {
+        throw runtime_error(op + " is not a valid instruction.");
+    }
+}
+
+void readcode (ifstream &fin, vector<string> &oper, vector<string> &registers) {
     char s;
     string op, reg;
-    int add=0;
     while (fin >> noskipws >> s) {
         if (s == '#' || s == '.') {
             while (fin >> noskipws >> s && s != '\n');
         } else if (s != '\n' && s != ' ' && s != '\t' && s != '#') {
             op = "";
             reg = "";
+            s = toupper(s);
             op += s;
             while (fin >> noskipws >> s && s != ' ' && s != '\t' && s != '\n' && s != '#') {
+                s = toupper(s);
                 op += s;
             }
             
-            if (op != "NOP" && op != "SETC" && op != "CLRC" && op != "RET" && op != "RTI"){
+            if (op != "NOP" && op != "SETC" && op != "CLRC" && op != "RET" && op != "RTI") {
                 while (fin >> noskipws >> s && s != '\n' && s != ' ' && s != '\t' && s != '#') {
                     reg += s;
                 }
             }
             if (op == "")
                 continue;
+            
+            checksyntax(op);
             cout << op << " " << reg << endl;
-            fout<<add++<<": ";
-            if (op == "NOP") {
-                fout << "0000000000000000" << endl;
-            } else if (op == "SETC") {
-                fout << "0000100000000000" << endl;
-            } else if (op == "CLRC") {
-                fout << "0001000000000000" << endl;
-            } else if (op == "NOT") {
-                fout << "00011" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string() << "00000"
-                     << endl;
-            } else if (op == "INC") {
-                fout << "00100" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string() << "00000"
-                     << endl;
-            } else if (op == "DEC") {
-                fout << "00101" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string() << "00000"
-                     << endl;
-            } else if (op == "OUT") {
-                fout << "00110000" << bitset<3>(reg[1] - '0').to_string() << "00000" << endl;
-            } else if (op == "IN") {
-                fout << "00111" << bitset<3>(reg[1] - '0').to_string() << "00000000" << endl;
-            } else if (op == "MOV") {
-                fout << "01000" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string() << "00000"
-                     << endl;
-            } else if (op == "ADD") {
-                fout << "01001" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string()
-                     << bitset<3>(reg[7] - '0').to_string() << "00" << endl;
-            } else if (op == "SUB") {
-                fout << "01010" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string()
-                     << bitset<3>(reg[7] - '0').to_string() << "00" << endl;
-            } else if (op == "AND") {
-                fout << "01011" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string()
-                     << bitset<3>(reg[7] - '0').to_string() << "00" << endl;
-            } else if (op == "OR") {
-                fout << "01100" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string()
-                     << bitset<3>(reg[7] - '0').to_string() << "00" << endl;
-            } else if (op == "PUSH") {
-                fout << "01101000" << bitset<3>(reg[1] - '0').to_string() << "00000" << endl;
-            } else if (op == "POP") {
-                fout << "01110" << bitset<3>(reg[1] - '0').to_string() << "00000000" << endl;
-            } else if (op == "LDD") {
-                fout << "01111" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string() << "00000"
-                     << endl;
-            } else if (op == "STD") {
-                fout << "10000000" << bitset<3>(reg[4] - '0').to_string() << bitset<3>(reg[1] - '0').to_string()
-                     << "00" << endl;
-            } else if (op == "JZ") {
-                fout << "10001" << bitset<3>(reg[1] - '0').to_string() << "00000000" << endl;
-            } else if (op == "JC") {
-                fout << "10010" << bitset<3>(reg[1] - '0').to_string() << "00000000" << endl;
-            } else if (op == "JMP") {
-                fout << "10011" << bitset<3>(reg[1] - '0').to_string() << "00000000" << endl;
-            } else if (op == "CALL") {
-                fout << "10100" << bitset<3>(reg[1] - '0').to_string() << "00000000" << endl;
-            } else if (op == "RET") {
-                fout << "1010100000000000" << endl;
-            } else if (op == "RTI") {
-                fout << "1011000000000000" << endl;
-            } else if (op == "IADD") {
-                fout << "1" << bitset<3>(reg[1] - '0').to_string() << bitset<3>(reg[4] - '0').to_string() << "00000"
-                     << endl;
-                fout << hex2bin(reg[6]) + hex2bin(reg[7]) + hex2bin(reg[8]) + hex2bin(reg[9]) << endl;
-            }
+            
+            oper.push_back(op);
+            registers.push_back(reg);
+            
         }
     }
+}
+
+void reordering (vector<string> &oper, vector<string> &registers) {
+    for (int i = 0; i < oper.size(); i++) {
+        if (oper[i] == "JZ" || oper[i] == "JC" || oper[i] == "JMP" || oper[i] == "CALL") {
+            int j = i + 1;
+            while (oper[j] != "NOP") {
+                j++;
+            }
+            swap(oper[i], oper[j]);
+            swap(registers[i], registers[j]);
+        }
+    }
+}
+
+int main (int argc, char *argv[]) {
+    ifstream fin(argv[1] + string(".asm"));
+    ofstream fout(argv[2] + string(".mem"));
+    
+    vector<string> oper;
+    vector<string> registers;
+    
+    readcode(fin, oper, registers);
+    reordering(oper, registers);
+    encode(oper, registers, fout);
+    
     return 0;
 }
