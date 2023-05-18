c9ab4bd7629962263e3be78ba8956d66cd08c1f7
حسين مصطفي محمد صلاح الدين <14712020101885@stud.cu.edu.eg>
2023-05-18 23:07:19 +0300

    Structural_Hazard_detection

---
+++ Structural_hazard_detection.vhd
@@ -0,0 +1,26 @@
+Library ieee;
+use ieee.std_logic_1164.all;
+USE IEEE.numeric_std.all;
+entity Load_Use is
+    port (
+        MemReadAlu: in std_logic;--coming from the Alu buffer control signal
+        MemWriteAlu: in std_logic;--coming from the Alu buffer control signal
+        MemReadMem1: in std_logic;--coming from the Mem1 buffer control signal
+        MemWriteMem1: in std_logic;--coming from the Mem1 buffer control signals
+        stall: out std_logic
+    );
+end Load_Use;
+
+architecture Load_UseArch of Load_Use is
+begin
+process(MemReadAlu,MemWriteAlu,MemReadMem1,MemWriteMem1)
+variable opCodeint:integer;
+begin
+    if((MemReadAlu=1 or MemWriteAlu=1) and (MemReadMem1=1 or MemWriteMem1=1)) then
+        stall<='1';
+    else
+        stall<='0';
+    end if;
+end process;
+
+end architecture;
